c5cdd94fcfc1de423755c78937d068a53d2f3e3c
lalaantika sharma <sharmal0707@gmail.com>
2021-02-24 17:07:09 -0500

    project working with Ai

---
+++ TicTacToe/TictactoeAi/TictactoeAi/ViewController.swift
@@ -0,0 +1,322 @@
+//
+//  ViewController.swift
+//  TictactoeAi
+//
+//  Created by student on 2021-02-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var ticTacImg1: UIImageView!
+    @IBOutlet var ticTagImg2: UIImageView!
+    @IBOutlet var ticTagImg3: UIImageView!
+    @IBOutlet var ticTagImg4: UIImageView!
+    @IBOutlet var ticTagImg5: UIImageView!
+    @IBOutlet var ticTagImg6: UIImageView!
+    @IBOutlet var ticTagImg7: UIImageView!
+    @IBOutlet var ticTagImg8: UIImageView!
+    @IBOutlet var ticTagImg9: UIImageView!
+    
+    @IBOutlet var ticTacBtn1: UIButton!
+    @IBOutlet var ticTacBtn2: UIButton!
+    @IBOutlet var ticTacBtn3: UIButton!
+    @IBOutlet var ticTacBtn4: UIButton!
+    @IBOutlet var ticTacBtn5: UIButton!
+    @IBOutlet var ticTacBtn6: UIButton!
+    @IBOutlet var ticTacBtn7: UIButton!
+    @IBOutlet var ticTacBtn8: UIButton!
+    @IBOutlet var ticTacBtn9: UIButton!
+    
+    @IBOutlet var resetBtn: UIButton!
+    @IBOutlet var userMessage: UILabel!
+    
+    var plays = Dictionary<Int,Int>()
+    var done = false
+    var aiDeciding = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func UIButtonClicked(_ sender: UIButton) {
+        userMessage.isHidden = true
+        if (plays[sender.tag] == nil) && !aiDeciding && !done{
+            setImageForSpot(spot: sender.tag, player:1)
+        }
+        checkForWin()
+        aiTurn()
+    }
+    
+    @IBAction func resetBtnClicked(_ sender: UIButton) {
+        done = false
+        resetBtn.isHidden = true
+        userMessage.isHidden = true
+        reset()
+        
+    }
+    
+    func setImageForSpot(spot:Int, player:Int){
+        let playerMark = player == 1 ? "x" : "o"
+        plays[spot] = player
+        switch spot {
+        case 1:
+            ticTacImg1.image = UIImage(named: playerMark)
+        case 2:
+            ticTagImg2.image = UIImage(named: playerMark)
+        case 3:
+            ticTagImg3.image = UIImage(named: playerMark)
+        case 4:
+            ticTagImg4.image = UIImage(named: playerMark)
+        case 5:
+            ticTagImg5.image = UIImage(named: playerMark)
+        case 6:                          ticTagImg6.image = UIImage(named: playerMark)
+        case 7:
+            ticTagImg7.image = UIImage(named: playerMark)
+        case 8:
+            ticTagImg8.image = UIImage(named: playerMark)
+        case 9:
+        ticTagImg9.image = UIImage(named: playerMark)
+        default:
+            ticTagImg5.image = UIImage(named: playerMark)
+        }
+    }
+    
+    
+    func reset(){
+        plays = [:]
+        ticTacImg1.image = nil
+        ticTagImg2.image = nil
+        ticTagImg3.image = nil
+        ticTagImg4.image = nil
+        ticTagImg5.image = nil
+        ticTagImg6.image = nil
+        ticTagImg7.image = nil
+        ticTagImg8.image = nil
+        ticTagImg9.image = nil
+    }
+    func checkForWin(){
+        let whoWon = ["I":0, "you":1]
+        for(key,value) in whoWon{
+            if ((plays[7] == value && plays[8] == value && plays[9] == value ) ||  //across the bottom
+                (plays[4] == value && plays[5] == value && plays[6] == value ) || //across the middle
+                (plays[1] == value && plays[2] == value && plays[3] == value ) || //across the top
+                (plays[1] == value && plays[4] == value && plays[7] == value ) || //down to left
+                (plays[2] == value && plays[5] == value && plays[8] == value ) || //down to middle
+                (plays[9] == value && plays[6] == value && plays[3] == value ) || //down to right
+                (plays[1] == value && plays[5] == value && plays[9] == value ) || //diag left right
+                (plays[3] == value && plays[5] == value && plays[7] == value )) //diag right to left
+            {
+                userMessage.isHidden = false
+                userMessage.text = "Looks like \(key) won!"
+                resetBtn.isHidden = false
+                done = true
+            }
+            
+        }
+        
+    }
+    
+    func checkBottom(value:Int)-> (location:String,pattern:String){
+        return ("bottom",checkFor(value: value, inList:[7,8,9]))
+    }
+    func checkTop(value:Int)-> (location:String,pattern:String){
+        return ("top",checkFor(value: value, inList:[1,2,3]))
+    }
+    func checkMiddleAcross(value:Int)-> (location:String,pattern:String){
+        return ("middleHorz",checkFor(value: value, inList:[4,5,6]))
+    }
+    func checkLeft(value:Int)-> (location:String,pattern:String){
+        return ("left",checkFor(value: value, inList:[1,4,7]))
+    }
+    func checkRight(value:Int)-> (location:String,pattern:String){
+        return ("right",checkFor(value: value, inList:[9,6,3]))
+    }
+    func checkMiddleDown(value:Int)-> (location:String,pattern:String){
+        return ("middleVert",checkFor(value: value, inList:[2,5,8]))
+    }
+    func checkDiagLeftRight(value:Int)-> (location:String,pattern:String){
+        return ("diagLeftRight",checkFor(value: value, inList:[1,5,9]))
+    }
+    func checkDiagRightLeft(value:Int)-> (location:String,pattern:String){
+        return ("diagRightLeft",checkFor(value: value, inList:[3,5,7]))
+    }
+    func checkFor(value:Int, inList:[Int]) -> String {
+        var conclusion = ""
+        for cell in  inList {
+            if plays[cell] == value{
+                conclusion += "1"
+            }else{
+                conclusion += "0"
+            }
+        }
+        return conclusion
+    }
+    
+    func rowCheck(value:Int)->(location:String,pattern:String)?{
+        let acceptableFinds = ["011", "110", "101"]
+        let findFuncs = [checkTop, checkBottom ,checkLeft, checkRight, checkMiddleAcross, checkMiddleDown, checkDiagLeftRight, checkDiagRightLeft]
+        for algorithm in findFuncs{
+            let algorithmResults = algorithm(value)
+            if acceptableFinds.contains(algorithmResults.pattern){
+                return algorithmResults
+            }
+        }
+        return nil
+    }
+    
+    
+    func isOccupied(spot:Int) -> Bool{
+        if plays[spot] == nil{
+            return false
+        }
+        else{
+        return true
+        }
+    }
+    
+    func aiTurn(){
+        if done{
+            return
+        }
+        aiDeciding = true
+        if let result = rowCheck(value:0){
+            let whereToPlayResult = whereToPlay(location: result.location, pattern: result.pattern)
+            if !isOccupied(spot: whereToPlayResult){
+                setImageForSpot(spot: whereToPlayResult, player: 0)
+                aiDeciding = false
+                checkForWin()
+                return
+            }
+            
+        }
+        
+        if let result = rowCheck(value:0){
+            let whereToPlayResult = whereToPlay(location: result.location, pattern: result.pattern)
+            if !isOccupied(spot: whereToPlayResult){
+                setImageForSpot(spot: whereToPlayResult, player: 0)
+                aiDeciding = false
+                checkForWin()
+                return
+            }
+        }
+        
+        if !isOccupied(spot: 5){
+            setImageForSpot(spot: 5, player: 0)
+            aiDeciding = false
+            checkForWin()
+            return
+        }
+        
+        
+        func firstAvailable(isCorner:Bool)-> Int? {
+            let spots = isCorner ? [1,3,7,9] : [2,4,6,8]
+            for spot in spots {
+                if !isOccupied(spot: spot){
+                    return spot
+                }
+            }
+            return nil
+        }
+        // corner Avaiable
+        
+        if let cornerAvailable = firstAvailable(isCorner: true){
+            setImageForSpot(spot: cornerAvailable, player: 0)
+            aiDeciding = false
+            checkForWin()
+            return
+        }
+        
+        if let sideAvailable = firstAvailable(isCorner: false){
+            setImageForSpot(spot: sideAvailable, player: 0)
+            aiDeciding = false
+            checkForWin()
+            return
+        }
+        
+        
+        userMessage.isHidden = false
+        userMessage.text = "Looks Like its a Draw"
+        reset()
+        aiDeciding = false
+    }
+    
+    func whereToPlay(location:String, pattern: String)-> Int {
+        let leftPattern = "011"
+        let rightPattern = "110"
+        _ = "101"
+        switch location {
+        case "top":
+            if pattern == leftPattern{
+                return 1
+            }else if pattern == rightPattern{
+                return 3
+            }else {
+                return 2
+            }
+        case "bottom":
+            if pattern == leftPattern{
+                return 7
+            }else if pattern == rightPattern{
+                return 9
+            }else {
+                return 8
+            }
+
+        case "left":
+            if pattern == leftPattern{
+                return 1
+            }else if pattern == rightPattern{
+                return 7
+            }else {
+                return 4
+            }
+        case "right":
+            if pattern == leftPattern{
+                return 3
+            }else if pattern == rightPattern{
+                return 9
+            }else {
+                return 6
+            }
+        case "middleVert":
+            if pattern == leftPattern{
+                return 2
+            }else if pattern == rightPattern{
+                return 8
+            }else {
+                return 5
+            }
+        case "middleHorz":
+            if pattern == leftPattern{
+                return 4
+            }else if pattern == rightPattern{
+                return 6
+            }else {
+                return 5
+            }
+        case "diagRightLeft":
+            if pattern == leftPattern{
+                return 3
+            }else if pattern == rightPattern{
+                return 7
+            }else {
+                return 5
+            }
+        case "diagLeftRight":
+            if pattern == leftPattern{
+                return 1
+            }else if pattern == rightPattern{
+                return 9
+            }else {
+                return 5
+            }
+        default: return 4
+        }
+    }
+
+}
+
